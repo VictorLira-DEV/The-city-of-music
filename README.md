bb2681e49f3a97e58a733f36022cdb87d7d7322c
VictorLira-DEV <victorliradev@gmail.com>
2021-03-15 19:51:44 -0300

    music player

---
+++ README.md
@@ -0,0 +1,47 @@
+# The City of Music
+
+<h1 align="center" >
+    <img src="https://ik.imagekit.io/Victorliradev/Animated_GIF-downsized_large_dnaVBerq6.gif">
+</h1>
+
+# Sections
+
+- [about](#-About)
+- [technologies](#-technologies)
+- [Contributing](#-Contributing)
+
+
+## :mag:  About
+This project is called the city of music, you can listen to some music that I like :), 
+its front-end project which means all the information of songs, artists, the band are stored in the front-end, I made this project on my own to finally put sass skills into work.
+
+---
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+All the services were TDD planed, so please make sure to update tests as appropriate.
+
+---
+
+## 🛠 technologies
+the project was developed using the following technologies 
+
+- [SASS](https://sass-lang.com/)
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [JAVASCRIPT](https://www.javascript.com/)
+
+## Dark mode
+
+<h1 align="center" >
+    <img src="https://ik.imagekit.io/Victorliradev/Animated_GIF-downsized_large__1__PxY1HsFt8.gif">
+</h1>
+
+:key: Run the application
+Clone this repository $ git clone https://github.com/raissaboeng/landing-page-masterclass.git
+
+Open the project folder on your machine and run the index.html file in your browser.
+
+Created by **Victor lira dev**
---
+++ main.js
@@ -0,0 +1,102 @@
+document.querySelector('.btn-starter').addEventListener('click', goRandom);
+document.querySelector('.popUp button').addEventListener('click', removeModal);
+
+const btnAbout = document.querySelector('#about-music').addEventListener('click', displayModal)
+const audio = document.querySelector('div audio')
+const images = document.querySelector('.images')
+const popUpPicture = document.querySelector('.popUp-image');
+const artist = document.querySelector('#artist')
+const songName = document.querySelector('#song-name')
+const release = document.querySelector('#release')
+
+let picture = document.createElement('img');
+
+function goRandom() {
+    const displayAboutButton = document.querySelector('#about-music');
+    const currentPlay = document.querySelector('.current-play');
+    const currentName = document.querySelector('#currentName');
+    const currentSong = document.querySelector('#currentSong');
+    const img = document.querySelector('.images img');
+    const randomMusic = Math.floor(Math.random() * 6 + 1);
+    displayAboutButton.classList.remove('hide');
+    currentPlay.classList.remove('hide')
+    
+    artist.innerHTML = ''
+    songName.innerHTML = ''
+    release.innerHTML = ''
+
+    audio.setAttribute('src', `./audio/${randomMusic}.mp3`)
+    img.setAttribute('src', `./imagens/${randomMusic}.png`)
+    img.setAttribute('width', '100%')
+    img.setAttribute('height', '100%')
+    images.appendChild(img)
+
+    picture.setAttribute('src', `./imagens/${randomMusic}.png`)
+    picture.setAttribute('width', '100%')
+    picture.setAttribute('height', '100%')
+    popUpPicture.appendChild(picture)
+
+    const musicas = [
+        {},
+        {
+            name: ' a7x  ',
+            song: 'Afterlife', 
+            year: 2007
+        },
+        {
+            name: ' a7x ',
+            song: 'The stage', 
+            year: 2016
+        },
+        {
+            name: '30 s.t.m',
+            song: 'Hurricane', 
+            year: 2009
+        },
+        {
+            name: '30 s.t.m',
+            song: 'Up in the air', 
+            year: 2013 
+        },
+        {
+            name: 'Eminem',
+            song: 'Godzilla', 
+            year: 2020
+        },
+         {
+            name: 'Eminem',
+            song: '8mile', 
+            year: 2003
+        },
+    ]
+
+    currentName.innerHTML = musicas[randomMusic].name
+    currentSong.innerHTML = musicas[randomMusic].song
+
+    artist.innerHTML += ` ${musicas[randomMusic].name}`
+    songName.innerHTML += ` ${musicas[randomMusic].song}`
+    release.innerHTML += ` ${musicas[randomMusic].year}`
+
+};  
+
+function displayModal() {
+    const overlay = document.querySelector('.overlay');
+    const popUp = document.querySelector('.popUp');
+    overlay.classList.add('active')
+    popUp.classList.add('active')
+};
+
+function removeModal() {
+    const overlay = document.querySelector('.overlay');
+    const popUp = document.querySelector('.popUp');
+    overlay.classList.remove('active')
+    popUp.classList.remove('active')
+}
+
+//dark mode
+const checkbox = document.querySelector('.checkbox');
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+    document.body.querySelector('.container').classList.toggle('dark')
+    document.body.querySelector('.popUp').classList.toggle('dark')
+})
